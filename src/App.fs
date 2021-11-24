2b6bf39a060056230bb23d6d9ad5856cf0cad5d7
Phil de Joux <philderbeast@gmail.com>
2021-11-24 07:29:48 -0500

    Use Feliz.Router and set comp prefix.

--- src/App.fs
+++ src/App.fs
@@ -1,15 +1,32 @@
 module App
 
-open Browser.Dom
 open Feliz
+open Feliz.Router
+
+let (|StringSegment|_|) (input: string) =
+    if System.String.IsNullOrWhiteSpace input && not (input.Contains "/") then None else Some input
 
 type Components =
     [<ReactComponent(import="Index", from="./Index.jsx")>]
     static member Index () = React.imported()
 
 [<ReactComponent>]
-let App () = React.fragment [
-    Components.Index()
-  ]
+let Router() =
+    let (url, setUrl) = React.useState(Router.currentUrl())
+    let (compPrefix, setCompPrefix) = React.useState("2017-dalby")
+    React.router [
+        router.onUrlChanged setUrl
+        router.children [
+            match url with
+            | [ ] -> Components.Index()
+            | [ "comp" ] -> Html.h1 ("comp page " + compPrefix)
+            | [ "comp-prefix"; StringSegment compPrefix ] -> setCompPrefix compPrefix; Router.navigate "comp"
+            | [ "settings" ] -> Html.h1 "settings page"
+            | [ "pilots" ] -> Html.h1 "pilots page"
+            | otherwise -> Html.h1 "Not found"
+        ]
+    ]
 
-ReactDOM.render (App(), document.getElementById "feliz")
+open Browser.Dom
+
+ReactDOM.render (Router(), document.getElementById "feliz")
--- src/Index.jsx
+++ src/Index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 export const Index = () => {
-    const [compPrefix, setCompPrefix] = React.useState("2017-dalby");
     return (
         <div id="content" className="content">
-            <p>{compPrefix}</p>
             <div className="tile is-ancestor">
                 <div className="tile is-parent">
                     <article className="tile is-child notification is-light">
@@ -29,12 +27,12 @@ export const Index = () => {
                                 <ul>
                                     <li>
                                         Italian Open
-                                        {' '}<a onClick={() => setCompPrefix('2020-italy-open')}>2020</a>
+                                        {' '}<a href='#/comp-prefix/2020-italy-open'>2020</a>
                                     </li>
                                     <li>
                                         Dalmatian
-                                        {' '}<a onClick={() => setCompPrefix('2019-dalmatian')}>2019</a>
-                                        {' '}<a onClick={() => setCompPrefix('2018-dalmatian')}>2018</a>
+                                        {' '}<a href='#/comp-prefix/2019-dalmatian'>2019</a>
+                                        {' '}<a href='#/comp-prefix/2018-dalmatian'>2018</a>
                                     </li>
                                     <ul>
                                         <p />
@@ -46,10 +44,10 @@ export const Index = () => {
                                 <p />
                                 <ul>
                                     <li>
-                                        <a onClick={() => setCompPrefix('1976-never-land')}>1976 Never Land</a>
+                                        <a href='#/comp-prefix/1976-never-land'>1976 Never Land</a>
                                     </li>
                                     <li>
-                                        <a onClick={() => setCompPrefix('1989-lift-lines')}>1989 Lift Lines</a>
+                                        <a href='#/comp-prefix/1989-lift-lines'>1989 Lift Lines</a>
                                     </li>
                                     <ul>
                                         <p />
@@ -68,40 +66,39 @@ export const Index = () => {
                                 <ul>
                                     <li>
                                         Forbes Flatlands
-                                        {' '}<a onClick={() => setCompPrefix('2018-forbes')}>2018</a>
-                                        {' '}<a onClick={() => setCompPrefix('2017-forbes')}>2017</a>
-                                        {' '}<a onClick={() => setCompPrefix('2016-forbes')}>2016</a>
-                                        {' '}<a onClick={() => setCompPrefix('2015-forbes')}>2015</a>
-                                        {' '}<a onClick={() => setCompPrefix('2014-forbes')}>2014</a>
-                                        {' '} <a onClick={() => setCompPrefix('2012-forbes')}>2012</a>
+                                        {' '}<a href='#/comp-prefix/2018-forbes'>2018</a>
+                                        {' '}<a href='#/comp-prefix/2017-forbes'>2017</a>
+                                        {' '}<a href='#/comp-prefix/2016-forbes'>2016</a>
+                                        {' '}<a href='#/comp-prefix/2015-forbes'>2015</a>
+                                        {' '}<a href='#/comp-prefix/2014-forbes'>2014</a>
+                                        {' '} <a href='#/comp-prefix/2012-forbes'>2012</a>
                                     </li>
                                     <li>
-                                        Dalby Big Air <a onClick={() => setCompPrefix('2017-dalby')}>2017</a>
+                                        Dalby Big Air <a href='#/comp-prefix/2017-dalby'>2017</a>
                                     </li>
                                 </ul>
                                 <h5>Europe</h5>
                                 <ul>
                                     <li>
-                                        Meduno <a onClick={() => setCompPrefix('2020-meduno')}>2020</a>
+                                        Meduno <a href='#/comp-prefix/2020-meduno'>2020</a>
                                     </li>
                                     <li>
-                                        Tolmezzo <a onClick={() => setCompPrefix('2019-italy')}>2019</a>
+                                        Tolmezzo <a href='#/comp-prefix/2019-italy'>2019</a>
                                     </li>
                                 </ul>
                                 <h5>Americas</h5>
                                 <ul>
                                     <li>
                                         Green Swamp Klassic 2016
-                                        {' '}<a onClick={() => setCompPrefix('2016-greenswamp')}>Topless</a>
-                                        {' '}<a onClick={() => setCompPrefix('2016-greenswamp-sport')}
-                                        >Kingposted</a
+                                        {' '}<a href='#/comp-prefix/2016-greenswamp'>Topless</a>
+                                        {' '}<a href='#/comp-prefix/2016-greenswamp-sport'>Kingposted</a
                                         >
                                     </li>
                                     <li>
-                                        Big Spring <a onClick={() => setCompPrefix('2016-big-spring')}>2016</a>
+                                        Big Spring <a href='2016-big-spring'>2016</a>
                                     </li>
                                     <li>
-                                        QuestAir Open <a onClick={() => setCompPrefix('2016-quest')}>2016</a>
+                                        QuestAir Open <a href='#/comp-prefix/2016-quest'>2016</a>
                                     </li>
                                 </ul>
                             </div>
